40b889aa4006effbbec86f9cceb7e9d984b39523
Dayeon Lee <43718982+sprituz@users.noreply.github.com>
2023-08-13 23:16:05 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 13.06 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] strArr = s.split(" ");
+        int[] intArr = new int[strArr.length];
+        for (int i = 0; i < strArr.length; i++) {
+            intArr[i] = Integer.parseInt(strArr[i]);
+        }
+        Arrays.sort(intArr);
+        return Integer.toString(intArr[0]) + " " + Integer.toString(intArr[intArr.length-1]);
+    }
+}
